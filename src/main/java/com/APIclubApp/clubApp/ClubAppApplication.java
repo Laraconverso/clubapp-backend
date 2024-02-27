d29a5da6a4801fb7382c83fe73fc441791ff747e
Matias Menoni <rookiemat@gmail.com>
2024-02-27 13:11:53 -0300

    adding authorizartionFilter

--- src/main/java/com/APIclubApp/clubApp/ClubAppApplication.java
+++ src/main/java/com/APIclubApp/clubApp/ClubAppApplication.java
@@ -1,5 +1,11 @@
 package com.APIclubApp.clubApp;
 
+import com.APIclubApp.clubApp.model.UserSecurityModels.ERole;
+import com.APIclubApp.clubApp.model.UserSecurityModels.RoleEntity;
+import com.APIclubApp.clubApp.model.UserSecurityModels.UserEntity;
+import com.APIclubApp.clubApp.repository.UserSecurityRepositories.UserSecurityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class ClubAppApplication {
@@ -18,6 +26,29 @@ public class ClubAppApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
 			}
+		};
+	}
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserSecurityRepository userSecurityRepository;
+
+	@Bean
+	CommandLineRunner init(){
+
+		return args -> {
+			UserEntity adminUser = UserEntity.builder()
+					.email("admin@mail.com")
+					.username("admin")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.ADMIN.name()))
+							.build()))
+					.build();
+
+			userSecurityRepository.save(adminUser);
 		};
 	}
 
--- src/main/java/com/APIclubApp/clubApp/config/security/SecurityConfig.java
+++ src/main/java/com/APIclubApp/clubApp/config/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.APIclubApp.clubApp.config.security;
 
+import com.APIclubApp.clubApp.security.filters.JwtAuthenticationFilter;
+import com.APIclubApp.clubApp.security.filters.JwtAuthorizartionFilter;
+import com.APIclubApp.clubApp.security.jwt.JwtUtils;
+import com.APIclubApp.clubApp.service.impl.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,17 +12,33 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    JwtAuthorizartionFilter authorizartionFilter;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils);
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
         return httpSecurity
                 .csrf(config -> config.disable())
                 .authorizeHttpRequests(auth -> {
@@ -27,20 +48,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
-                .httpBasic()
-                .and()
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(authorizartionFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
-    }
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("admin")
-                .password("1234")
-                .roles()
-                .build());
-        return manager;
     }
 
     @Bean
@@ -50,7 +61,7 @@ public class SecurityConfig {
     @Bean
     AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService())
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder)
                 .and().build();
     }
